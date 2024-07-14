eef9a9353af10c4a47e4b24593bfd266cc66eb11
DeZeroPourHero <DeZeroPourHero@gmail.com>
2024-07-14 18:19:14 +0200

    feat:recovery enter of users

---
+++ recovery.py
@@ -0,0 +1,7 @@
+prenom = input("Please enter your first name ")
+ville_naissance_= input(" Please enter your city of birth ")
+age = input(" Please enter your age ")
+
+print(prenom)
+print(ville_naissance_)
+print(age)
